d32d0bdc7f7e22bb65937d6da6421465129da46a
mnescot <mnescot@gmail.com>
2021-03-07 13:09:12 -0800

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-52e67f2f897f935bae39.js"
+    "url": "webpack-runtime-8cd84a3a170a2489f278.js"
   },
   {
     "url": "framework-efd1c5c3af7258af61ab.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "b3cb9a802b205979a0c54a93e8b66f28"
+    "revision": "e1cf5c66184e215df0a5a71105f4a157"
   },
   {
     "url": "static/webfonts/s/montserrat/v15/JTURjIg1_i6t8kCHKm45_cJD3gnD_g.woff2"
